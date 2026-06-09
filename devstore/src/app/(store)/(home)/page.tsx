2641c0a974cf1725f8bc19de094c902b7f89e8fd
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-06-09 10:51:03 +0200

    setting home
    importing imags
    
    div with links and imgs

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -1,4 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return <div>Hello world!</div>;
+  return (
+    <div className="grid max-h-215 grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/moletom-ai-side.png"
+          alt="Products"
+          width={920}
+          height={920}
+          quality={100}
+          className="group-hover:scale-105 transition-transform duration-300"
+        />
+        <div className="absolute bottom-28 h-12 right-28 flex items-center gap-2 max-w-70 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom AI Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            $129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/moletom-java.png"
+          alt="Products"
+          width={920}
+          height={920}
+          quality={100}
+          className="group-hover:scale-105 transition-transform duration-300"
+        />
+        <div className="absolute bottom-10 h-12 right-10 flex items-center gap-2 max-w-70 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Jumper</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            $129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/camiseta-dowhile-2022.png"
+          alt="Products"
+          width={920}
+          height={920}
+          quality={100}
+          className="group-hover:scale-105 transition-transform duration-300"
+        />
+
+        <div className="absolute bottom-10 h-12 right-10 flex items-center gap-2 max-w-70 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">T-shirt dowhile 2022</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            $129
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
 }
