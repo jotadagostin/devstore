2b588fc57211ccc63f4d64bcfdb953a7aa991dc4
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-06-16 16:55:25 +0200

    setting context

--- devstore/src/app/(store)/layout.tsx
+++ devstore/src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
 import { Header } from "@/components/Header";
+import { CartProvider } from "@/context/cart-context";
 import { ReactNode } from "react";
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid  w-full max-w-400 grid-rows-app gap-5 px-8 py-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid  w-full max-w-400 grid-rows-app gap-5 px-8 py-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   );
 }
