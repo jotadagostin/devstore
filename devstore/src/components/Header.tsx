b54b55c804eb45ae82074606e7864ce36f0cccac
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-06-19 14:04:38 +0200

    setting input search

--- devstore/src/components/Header.tsx
+++ devstore/src/components/Header.tsx
@@ -1,7 +1,7 @@
-import { SearchIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { CardWidget } from "./Card-Widget";
+import { SearchForm } from "./Search-form";
 
 export function Header() {
   return (
@@ -10,13 +10,8 @@ export function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           devstore
         </Link>
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <SearchIcon className="w-5 h-5 text-zinc-500" />
-          <input
-            placeholder="Search products..."
-            className="bg-transparent flex-1 text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+
+        <SearchForm />
       </div>
       <div className="flex items-center gap-4">
         <CardWidget />
