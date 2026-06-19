976dc1dbfc0f71a7571faa4f8139dacd8739c004
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-06-19 16:46:26 +0200

    Update loading.tsx
    settind loadgin page
    design and skeleton

--- devstore/src/app/(store)/search/loading.tsx
+++ devstore/src/app/(store)/search/loading.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { Skeleton } from "@/components/Skeleton";
+import { useSearchParams } from "next/navigation";
+
 export default function SearchLoading() {
-  return <p>Carregando busca...</p>;
+  const searchParams = useSearchParams();
+  const query = searchParams?.get("q");
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Results for: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-95" />
+        <Skeleton className="h-95" />
+        <Skeleton className="h-95" />
+        <Skeleton className="h-95" />
+        <Skeleton className="h-95" />
+      </div>
+    </div>
+  );
 }
